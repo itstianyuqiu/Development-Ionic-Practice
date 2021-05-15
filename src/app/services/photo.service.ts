55b49ddaeee613778c37669d3fd45179a7a0c640
tqiu654 <tqiu654@aucklanduni.av.nz>
2021-05-15 17:23:20 +1200

    Saving Photos to the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,23 +1,77 @@
 import { Injectable } from '@angular/core';
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
-  CameraPhoto, CameraSource } from '@capacitor/core';
+import {
+  Plugins,
+  CameraResultType,
+  Capacitor,
+  FilesystemDirectory,
+  CameraPhoto,
+  CameraSource,
+} from '@capacitor/core';
 
 const { Camera, Filesystem, Storage } = Plugins;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
 
-  constructor() { }
+  private async savePicture(cameraPhoto: CameraPhoto) { 
+    const base64Data = await this.readAsBase64(cameraPhoto);
+    const fileName = new Date().getTime() + '.jpeg';
+  const savedFile = await Filesystem.writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: FilesystemDirectory.Data
+  });
+  return {
+    filepath: fileName,
+    webviewPath: cameraPhoto.webPath
+  };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;  
+  }
+  
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  public photos: Photo[] = [];
+
+  constructor() {}
 
   public async addNewToGallery() {
-    const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri, 
-      source: CameraSource.Camera, 
-      quality: 100 
+
+  const capturedPhoto = await Camera.getPhoto({
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera, 
+    quality: 100 
+  });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
     });
+
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
+
+  
+
 }
 
-
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
