cf662a91d309a54f96d1971820d34321591bbb4b
tqiu654 <tqiu654@aucklanduni.av.nz>
2021-05-15 17:47:43 +1200

    Rapid App Development with Live Reload

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { Photo, PhotoService } from '../services/photo.service';
+
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,7 +10,10 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  
+
+  constructor(public photoService: PhotoService, 
+    public actionSheetController: ActionSheetController) {}
 
 addPhotoToGallery() {
   this.photoService.addNewToGallery();
@@ -16,6 +21,28 @@ addPhotoToGallery() {
 
 async ngOnInit() {
   await this.photoService.loadSaved();
+}
+
+public async showActionSheet(photo: Photo, position: number) {
+  const actionSheet = await this.actionSheetController.create({
+    header: 'Photos',
+    buttons: [{
+      text: 'Delete',
+      role: 'destructive',
+      icon: 'trash',
+      handler: () => {
+        this.photoService.deletePicture(photo, position);
+      }
+    }, {
+      text: 'Cancel',
+      icon: 'close',
+      role: 'cancel',
+      handler: () => {
+        // Nothing to do, action sheet is automatically closed
+        }
+    }]
+  });
+  await actionSheet.present();
 }
 
 }
